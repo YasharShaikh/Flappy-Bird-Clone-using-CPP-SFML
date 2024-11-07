8f98a4b0d2d738337466ff7218a612ae7c1cb0bb
Yashar Shaikh <yashrsk6@gmail.com>
2024-11-07 14:46:44 +0530

    add: Main Menu State

---
+++ SFML Template/SFML Template/MainMenuState.cpp
@@ -0,0 +1,59 @@
+#include "MainMenuState.h"
+#include <sstream>
+#include <iostream>
+#include "DEFINATIONS.h"
+
+namespace flappybird
+{
+	flappybird::MainMenuState::MainMenuState(GameDataRef data) : _data(data)
+	{
+	}
+
+	void flappybird::MainMenuState::Init()
+	{
+		this->_data->assets.LoadTexture("Main Menu State Background", MAIN_MENU_BACKGROUND_FILEPATH);
+		this->_data->assets.LoadTexture("Title", GAME_TITLE_FILEPATH);
+		this->_data->assets.LoadTexture("Play Button", PLAY_BUTTON_FILEPATH);
+
+
+		_background.setTexture(this->_data->assets.GetTexture("Main Menu State Background"));
+		_tite.setTexture(this->_data->assets.GetTexture("Title"));
+		_playButton.setTexture(this->_data->assets.GetTexture("Play Button"));
+
+
+		_tite.setPosition((SCREEN_WIDTH / 2) - (_tite.getGlobalBounds().width / 2), (_tite.getGlobalBounds().height / 2));
+		_playButton.setPosition((SCREEN_WIDTH / 2) - (_playButton.getGlobalBounds().width / 2), (SCREEN_HEIGHT / 2) - (_playButton.getGlobalBounds().height / 2));
+	}
+
+	void flappybird::MainMenuState::HandleInput()
+	{
+		sf::Event event;
+		while (_data->window.pollEvent(event))
+		{
+			if (sf::Event::Closed == event.type)
+			{
+				_data->window.close();
+			}
+			if (this->_data->input.IsSpriteClicked(this->_playButton, sf::Mouse::Left, this->_data->window))
+			{
+				std::cout << "go to game screen" << std::endl;
+			}
+		}
+	}
+
+	void flappybird::MainMenuState::Update(float dt)
+	{
+	}
+
+	void flappybird::MainMenuState::Draw(float dt)
+	{
+		this->_data->window.clear();
+
+		this->_data->window.draw(this->_background);
+		this->_data->window.draw(this->_tite);
+		this->_data->window.draw(this->_playButton);
+
+		this->_data->window.display();
+	}
+
+}
--- SFML Template/SFML Template/SplashState.cpp
+++ SFML Template/SFML Template/SplashState.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "SplashState.h"
 #include "DEFINATIONS.h"
+#include "MainMenuState.h"
 
 namespace flappybird
 {
@@ -34,13 +35,15 @@ namespace flappybird
 	{
 		if (this->_clock.getElapsedTime().asSeconds() > SPLASH_STATE_SHOW_TIME)
 		{
+
+			_data->machine.AddState(stateRef(new MainMenuState(this->_data)), true);
 			std::cout << "Go to main menu" << std::endl;
 		}
 	}
 
 	void SplashState::Draw(float dt)
 	{
-		this->_data->window.clear(sf::Color::Red);
+		this->_data->window.clear();
 
 		this->_data->window.draw(this->_background);
 
